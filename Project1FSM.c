a7f64c22c0e259ec61ce54f054b7c63209b554ea
sdanthinne <sdanthinne@gmail.com>
2020-04-26 11:49:05 -0700

    added additional commenting

--- Project1FSM.c
+++ Project1FSM.c
@@ -155,7 +155,11 @@ uint8_t read_username_state(Sys *sys, uint8_t key_press)
 
 
 
-
+/**
+ * Process number state:
+ *  adds the number that was inputted to the second line
+ *  of the display
+ */
 uint8_t process_number_state(Sys *sys, uint8_t key_press)
 {
 
@@ -167,6 +171,7 @@ uint8_t process_number_state(Sys *sys, uint8_t key_press)
 
         // Build up ASCII code for character
         uint8_t number_pressed = get_number_pressed(sys->curr_input);
+        //manages the shift of the numbers
         sys->curr_user[sys->letter_counter] += (number_pressed) * pow(10, 2-(sys->number_counter));
         ++(sys->number_counter);
 
@@ -183,6 +188,12 @@ uint8_t process_number_state(Sys *sys, uint8_t key_press)
     return READ_PASSWORD_STATE; // Go here if writing password
 }
 
+/**
+ * Process ascii state:
+ *  processes the numbers that have already been
+ *  inputted through username/process number state
+ *
+ */
 uint8_t process_ascii_state(Sys *sys, uint8_t key_press)
 {
     sys->number_counter = 0; // reset number counter for next ascii letter
@@ -191,9 +202,9 @@ uint8_t process_ascii_state(Sys *sys, uint8_t key_press)
     write_char_4(sys->curr_user[sys->letter_counter]); // Write current letter of username to LCD
     ++(sys->letter_counter); // Letter has been added to screen
 
-    set_address_counter_4(1, 0);
-    write_string_4("    ");
-    set_address_counter_4(1, 0);
+    set_address_counter_4(1, 0);//move to the new line
+    write_string_4("    ");//makes room to align text
+    set_address_counter_4(1, 0);//move to the new line
     sys->prev_state = PROCESS_ASCII_STATE; // To remember where you came from
 
     return READ_USERNAME_STATE; // Go here when letter has been written to screen
@@ -202,6 +213,7 @@ uint8_t process_ascii_state(Sys *sys, uint8_t key_press)
 
 /**
  * compares passwords of length 5 at one and two
+ *  return: 1 if same, 0 if different
  */
 uint8_t compare_password(uint8_t * one,uint8_t *two)
 {
@@ -213,35 +225,38 @@ uint8_t compare_password(uint8_t * one,uint8_t *two)
     }
     return 1;
 }
+
 /**
- * reads in the pressed keypad button and stores it in the curr_pin
- * once the curr_pin is full, then
+ * Read password state:
+ *  reads in the pressed keypad button and stores it in the curr_pin
+ *  once the curr_pin is full, then compare to the already stored password
+ *  if wrong, then go to the incorrect state
+ *  else, logged in state
  */
 uint8_t read_password_state(Sys* sys,uint8_t keypress)
 {
 
     uint8_t curr_press = 0;
-
     if(sys->prev_state==READ_USERNAME_STATE||
-            sys->prev_state==INCORRECT_STATE)
+            sys->prev_state==INCORRECT_STATE)//if we are not already in the password state previously
     {
         clear_display_4();
         set_address_counter_4(0,0);
         write_string_4("password: ");//writes "password:" to the top row of the LCD
         sys->prev_state=READ_PASSWORD_STATE;
     }
-    curr_press=update_key_press(curr_press);
+    curr_press=update_key_press(curr_press);//poll for a keypress
     sys->curr_input = curr_press;
     sys->curr_pin[sys->number_counter] =
             get_number_pressed(sys->curr_input);//store the pressed number into the current pin
     sys->prev_state=READ_PASSWORD_STATE;//sets the previous state
 
-
-    if(sys->number_counter<4)
+    if(sys->number_counter<4)//if we don't have all the numbers, then get some more
     {
         return PROCESS_NUMBER_STATE;//go to the process number state
     }
-    if(sys->number_counter==4)
+
+    if(sys->number_counter==4)//once we have all 5 numbers, check for validity
     {
         sys->number_counter=0;
         if(compare_password(
@@ -253,6 +268,7 @@ uint8_t read_password_state(Sys* sys,uint8_t keypress)
         }
         else
         {
+            //incorrect auth
             return INCORRECT_STATE;
         }
     }
@@ -260,31 +276,54 @@ uint8_t read_password_state(Sys* sys,uint8_t keypress)
 
 }
 
+/**
+ * Incorrect state:
+ *  depends on which state we came from to do an action
+ *  displays the error message
+ *  goes back to a respective function to re-enter input
+ */
 uint8_t incorrect_state(Sys* sys,uint8_t keypress)
 {
     uint8_t i;
-    clear_display_4();
-    write_string_4("get out that's wrong");
+    clear_display_4();//make sure the display is clear so we can write to it
+    write_string_4("get out that's wrong");//display our error message
     delay_us(3000000);//delays 3 seconds
-    clear_display_4();
-    for (i = 0; i < 4; i++) { // intialize current user array values to 0
+    clear_display_4();//clears the display from the error message
+
+    for (i = 0; i < 4; i++)
+    { // intialize current user array values to 0
         sys->curr_user[i] = 0;
     }
 
+    /*
+     * if we came from the username state,
+     * then we go to the init state
+     */
     if(sys->prev_state==READ_USERNAME_STATE)
     {
         sys->prev_state=INCORRECT_STATE;
         return INIT_STATE;
     }
+    /**
+     * if we came from the password state,
+     * then we go to back to the read password state
+     */
     if(sys->prev_state==READ_PASSWORD_STATE)
     {
         sys->prev_state=INCORRECT_STATE;
         return READ_PASSWORD_STATE;//go back to the password state if it was wrong
     }
+    //default case - should not be reached, but if so we go to init
     sys->prev_state=INCORRECT_STATE;
-    return INCORRECT_STATE;
+    return INIT_STATE;
 }
 
+/**
+ * Logged in state:
+ *  if we are not in the logged in state previously,
+ *  then print our login message
+ *  if the keypress is star, leave to init
+ */
 uint8_t logged_in_state(Sys* sys,uint8_t keypress)
 {
     if(sys->prev_state !=LOGGED_IN_STATE)
@@ -306,44 +345,48 @@ uint8_t logged_in_state(Sys* sys,uint8_t keypress)
 }
 
 /**
- * sets up the sys to have the sudo user
+ * sets up the sys
+ *  init display,keypad
+ *  init sudo user
  */
 void setup(Sys * sys)
 {
     uint8_t i;
-    uint8_t temp[5] = {8,0,0,8,5};
-    setup_lcd_4();
-    setup_keypad();
-    clear_display_4();
-    memcpy(sys->users[0].username,"sudo",sizeof(char)*4);
-//    sys->users[0].password[0]=8;
-//    sys->users[0].password[0]=0;
-//    sys->users[0].password[0]=0;
-//    sys->users[0].password[0]=8;
-//    sys->users[0].password[0]=5;
-    memcpy(sys->users[0].password,temp,sizeof(uint8_t)*5);
+    uint8_t temp[5] = {8,0,0,8,5};//initial "sudo user" password
+    setup_lcd_4();//start the lcd
+    setup_keypad();//start the keypad
+    clear_display_4();//clear whatever was on the keypad
+    memcpy(sys->users[0].username,"sudo",sizeof(char)*4);//copy over the username
+    memcpy(sys->users[0].password,temp,sizeof(uint8_t)*5);//copy over the password
 
-    for (i = 0; i < 4; i++) { // intialize current user array values to 0
+    for (i = 0; i < 4; i++)
+    { // intialize current user array values to 0
         sys->curr_user[i] = 0;
     }
 
-    sys->user_count=1;
+    sys->user_count=1;//now there's only one user
     sys->current_user_index=0;
     sys->letter_counter=0;
     sys->number_counter=0;
 }
 
+/**
+ * main of project 1
+ *  initializes with setup
+ *  starts the FSM at init
+ *  runs FSM
+ */
 void run_P1(void)
 {
     State * curr_state;
     uint8_t state_input;
-    uint8_t keypress=0;
+    uint8_t keypress=0; //we have no keypress
     Sys sys;
-    setup(&sys);
+    setup(&sys); //sets up the sys struct
     curr_state = INIT_STATE_LOC;//enter username
     state_input = (curr_state->state_function)(&sys,keypress);
-    curr_state = curr_state->next[state_input];
-    delay_us(50000);
+    curr_state = curr_state->next[state_input];//set the next state
+    delay_us(50000); //wait for a bit
     while(1)
     {
             keypress = get_key_pressed();    //get ASCII of the key pressed
--- main.c
+++ main.c
@@ -5,5 +5,5 @@
 void main(void)
 {
     WDT_A->CTL = WDT_A_CTL_PW | WDT_A_CTL_HOLD;     // stop watchdog timer
-    run_P1();
+    run_P1();//runs project 1
 }

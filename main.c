178063ec0ccaa784a84b1daecd2b5f5c3151c985
Alexander Knapen <alexander.w.knapen@gmail.com>
2020-04-30 16:06:35 -0700

    added initial main for A5

--- main.c
+++ main.c
@@ -1,9 +1,65 @@
+#include "timer.h"
+
+
+void setup_pin()
+{
+    // Set up output signal for ISR
+    P2->DIR |= BIT7; // configure P2.7 as output
+    P2->OUT |= BIT7;
+
+    P2->SEL0 &= ~BIT7; // configure P2.7 as GPIO
+    P2->SEL1 &= ~BIT7;
+
+    // Set up signal to time ISR
+    P2->DIR |= BIT5; // configure P2.5 as output
+    P2->OUT |= BIT5;
+
+    P2->SEL0 &= ~BIT5; // configure P2.5 as GPIO
+    P2->SEL1 &= ~BIT5;
+
+    // Set up signal to measure MCLK
+    P4->DIR |= BIT3; // configure P4.3 as output
+    P4->OUT |= BIT3;
+
+    P4->SEL0 |= BIT3; // configure P4.3 for primary function (MCLK)
+    P4->SEL1 &= ~BIT3;
+
+}
+
 /**
- * Main. used to call multiple projects
+ * main.c
  */
-#include "Project1FSM.h"
 void main(void)
 {
-    WDT_A->CTL = WDT_A_CTL_PW | WDT_A_CTL_HOLD;     // stop watchdog timer
-    run_P1();//runs project 1
+    WDT_A->CTL = WDT_A_CTL_PW | WDT_A_CTL_HOLD; // stop watchdog timer
+
+    __enable_irq(); // enable global interrupts
+    NVIC->ISER[0] = 1 << TA0_0_IRQn; // enable TimerA0's interrupts
+    NVIC->ISER[0] = 1 << TA0_N_IRQn; // enable TimerA1's interrupts
+
+	setup_pin();
+	set_timer();
+
+	while(1);
 }
+
+// Timer A0's CCR0 interrupt service routine
+void TA0_0_IRQHandler(void)
+{
+    P2->OUT |= BIT5; // set P2.5 high at beginning of ISR
+
+    TIMER_A0->CCTL[0] &= ~TIMER_A_CCTLN_CCIFG; // clears capture/compare interrupt flag
+    P2->OUT ^= BIT7; // toggles P2.7 when TA0_CCR0 reached
+
+
+    P2->OUT &=   ~BIT5; // clear P2.5 at the end of the ISR
+    // Modified CCR0 ISR for Part B of A5
+
+}
+
+// Timer A0's CCR1 interrupt service routine
+//void TA0_N_IRQHandler(void)
+//{
+//    TIMER_A0->CCTL[1] &= ~TIMER_A_CCTLN_CCIFG; // clears capture/compare interrupt flag
+//    P2->OUT &= ~BIT7; // sets P2.7 low when TA0_CCR1 reached
+//}

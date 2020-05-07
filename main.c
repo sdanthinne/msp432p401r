e62ebba861b70ca848f018c624a741b41c800263
sdanthinne <sdanthinne@gmail.com>
2020-05-06 20:28:19 -0700

    we now get a garbage sck signal

--- main.c
+++ main.c
@@ -11,12 +11,12 @@ void main(void)
     uint16_t data=1;
     uint16_t adder=2;
     WDT_A->CTL = WDT_A_CTL_PW | WDT_A_CTL_HOLD; // stop watchdog timer
+
     setDCO(FREQ_1_5_MHz);
     set_SM_DCO();
 
-    __disable_irq(); // disable global interrupts
+
     setup_SPI_b0();
-    __enable_irq(); //enable globular interrupts
 
 //    data = 0x3000;
 //    write_byte_b0(data>>8);
@@ -32,11 +32,10 @@ void main(void)
 //	    }
 //	    adder*=2;
 //	}
-    write_DAC(512);
-//    while(1)
-//    {
-//        write_DAC(data);
-//        data*=adder;
-//    }
+    //write_DAC(0xffff);
+    while(1)
+    {
+        write_DAC(0xffff);
+    }
     while(1);
 }
--- spi.c
+++ spi.c
@@ -17,37 +17,65 @@ void setup_SPI_b0()
     P4->DIR|=BIT1;//set to output
     P4->SEL0&=~BIT1;//set to GPIO
     P4->SEL1&=~BIT1;
+    P4->OUT&=~BIT1;
 
-    //P1->DIR|=BIT1;
-    P1->SEL0|=BIT6;
-    P1->SEL1&=~BIT6;//set to primary function (UCB0SIMO)
+    P1->DIR|=BIT6|BIT5;
+    P1->SEL0|=BIT6 | BIT5;//set to primary function (UCB0SIMO)(UCB0CLK)
 
-    //P1->DIR|=BIT1;
-    P1->SEL0|=BIT5;
-    P1->SEL1&=~BIT5;//set to primary function (UCB0CLK)
-
-
-    NVIC->ISER[0] |= (1<<(31&EUSCIB0_IRQn));//ENABLE INTERRUPTS !
     EUSCI_B0_SPI -> IE = UCTXIE;// ENABLE INTERRUPTS WITHIN SPI PROTOCOL!
+
+
+    __enable_irq(); //enable globular interrupts
+    NVIC->ISER[0] |= (1<<(31&(EUSCIB0_IRQn)));//ENABLE INTERRUPTS !
 
     EUSCI_B0_SPI->CTLW0 |= (
             EUSCI_B_CTLW0_MSB | //msb written first
             EUSCI_B_CTLW0_MST | //enable master mode
             EUSCI_B_CTLW0_SYNC | //enable synchronous mode
             EUSCI_B_CTLW0_SSEL__SMCLK | // set to use the smclk
-            EUSCI_B_CTLW0_MODE1); //3 pin spi
+            EUSCI_B_CTLW0_CKPL); //3 pin spi
 
-    EUSCI_B0_SPI->CTLW0 &= ~(
-            EUSCI_B_CTLW0_CKPL | //set to be active high
-            EUSCI_B_CTLW0_SEVENBIT | //set to 8 bit mode
-            EUSCI_B_CTLW0_MODE0 |//3 pin spi
-            EUSCI_B_CTLW0_SSEL0
-            );
-
-    EUSCI_B0_SPI->IFG &=~BIT1; //clear flags after hard reset
+//    EUSCI_B0_SPI->CTLW0 &= ~(
+//              //set to be active high
+//            EUSCI_B_CTLW0_SEVENBIT | //set to 8 bit mode
+//            EUSCI_B_CTLW0_MODE0 |//3 pin spi
+//            EUSCI_B_CTLW0_SSEL0
+//            );
+    EUSCI_B0->BRW = 0x00;//BREW
+//    EUSCI_B0_SPI->IFG &=~BIT1; //clear flags after hard reset
 
     EUSCI_B0_SPI->CTLW0 &= ~BIT0;//re-enable data transmission
 }
+
+
+
+/* This function initializes the spi on the board */
+//void init_SPI(void)
+//{
+//    EUSCI_B0 -> CTLW0 |= EUSCI_B_CTLW0_SWRST; // RESET STATE
+//
+//    EUSCI_B0 -> CTLW0 |= EUSCI_B_CTLW0_SWRST
+//                      |  EUSCI_B_CTLW0_CKPL     // polarity active low
+//                      |  EUSCI_B_CTLW0_MSB      // MSB set
+//                      |  EUSCI_B_CTLW0_MST      // SET AS MANAGER
+//                      |  EUSCI_B_CTLW0_SYNC     // ENABLE SYNCHRONOUS MODE
+//                      |  EUSCI_B_CTLW0_UCSSEL_2;// SET SMCLK
+//
+//    EUSCI_B0 -> BRW = 0X01; //SMCLK FULL SPEED
+//
+//
+//    P6 -> DIR |= MYCS;
+//    P6 -> OUT |= MYCS;
+//
+//    P1 -> SEL0 |= (BIT5 | BIT6); // 1.5 SCLK, 1.6 SIMO
+//    P1 -> SEL1 &= ~(BIT5 | BIT6);
+//
+//    P1 -> DIR |= (BIT5 | BIT6); // 1.5 SCLK, 1.6 SIMO
+//    P1 -> OUT |= (BIT5 | BIT6);
+//
+//    EUSCI_B0 -> CTLW0 &= ~(EUSCI_B_CTLW0_SWRST); // CLEAR RESET
+//
+//}
 
 
 /**
@@ -61,9 +89,9 @@ void write_byte_b0(uint8_t byte)
 
 void write_DAC(uint16_t bytes)
 {
-    write_byte_b0(0x3000|(bytes>>8));
+    write_byte_b0(0x30|(bytes>>8));
     while(EUSCI_B0_SPI->STATW&BIT0);
-    write_byte_b0_cont(0x3000|(bytes));
+    write_byte_b0_cont(bytes);
     while(EUSCI_B0_SPI->STATW&BIT0);
     P4->OUT|=BIT1;//set cs high
 }
@@ -76,7 +104,7 @@ void write_byte_b0_cont(uint8_t byte)
 void EUSCIB0_IRQHandler()
 {
     P4->OUT&=~BIT1;//set the cs to low
-    //EUSCI_B0_SPI->IFG&=~BIT1;//reset flag?
+    EUSCI_B0_SPI->IFG&=~BIT1;//reset flag?
 
 }
 

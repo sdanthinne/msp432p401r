071a98ab6a19a7fdda0369f25aa676ad2b5dd5c1
sdanthinne <sdanthinne@gmail.com>
2020-05-07 14:37:33 -0700

    formatting update

--- DAC.c
+++ DAC.c
@@ -17,7 +17,6 @@ void setup_DAC()
     set_SM_DCO();
     setup_SPI_b0();
     P4->OUT|=BIT1;//set cs high for default high
-
 }
 
 /**
@@ -29,14 +28,16 @@ void write_DAC(uint16_t bytes)
 {
     bytes = bytes & (0b1111111111);//select only the 10 bits
 
-    EUSCI_B0_SPI->IFG&=~BIT1;
+    EUSCI_B0_SPI->IFG&=~BIT1;//set the interrupt flag to low
 
     P4->OUT&=~BIT1;//set the cs to low
 
     write_byte_b0(0x30|(bytes>>6));//put the data into the txbuf with the correct settings
 
     write_byte_b0((bytes<<2));//write the next half of the byte
+
     while((EUSCI_B0_SPI->STATW&EUSCI_B_STATW_SPI_BUSY));//wait for the entire signal to be written
+
     P4->OUT|=BIT1;//set cs high again after signal has been written
 }
 
@@ -48,7 +49,7 @@ void write_SINE()
     uint16_t val=0;
     while(1)
     {
-        write_DAC(hreswave[val]);
+        write_DAC(wave[val]);
 
         val = (val+1)%100;
 
--- spi.c
+++ spi.c
@@ -13,17 +13,13 @@ void setup_SPI_b0()
 {
     EUSCI_B0_SPI->CTLW0 |= BIT0; //enable editing of settings/halt the data transmission
 
-    //need to configure ports
-    P4->DIR|=BIT1 | BIT2;//set to output
-    P4->SEL0&=~(BIT1 | BIT2);//set to GPIO
-    P4->SEL1&=~(BIT1 | BIT2);
-    P4->OUT&=~(BIT1 | BIT2);
+    P4->DIR|=BIT1;//set to output
+    P4->SEL0&=~(BIT1);//set to GPIO
+    P4->SEL1&=~(BIT1);
+    P4->OUT&=~(BIT1);
 
     P1->DIR|=BIT6|BIT5;
     P1->SEL0|=BIT6 | BIT5;//set to primary function (UCB0SIMO)(UCB0CLK)
-
-    //__enable_irq(); //enable global interrupts
-    //NVIC->ISER[0] |= (1<<(31&(EUSCIB0_IRQn)));//ENABLE INTERRUPTS !
 
     EUSCI_B0_SPI->CTLW0 |= (
             EUSCI_B_CTLW0_MSB | //msb written first
@@ -35,8 +31,6 @@ void setup_SPI_b0()
     EUSCI_B0_SPI->BRW = 0x00;//set the bitrate to go at SMCLK (0)
 
     EUSCI_B0_SPI->CTLW0 &= ~BIT0;//re-enable data transmission
-//    EUSCI_B0_SPI -> IE |= UCTXIE;// ENABLE INTERRUPTS WITHIN SPI PROTOCOL!
-//    EUSCI_B0_SPI->IFG &=~BIT1; //clear flags after hard reset
 }
 
 /**
@@ -44,12 +38,9 @@ void setup_SPI_b0()
  */
 void write_byte_b0(uint8_t byte)
 {
-    //questionable logic follows:
     while((EUSCI_B0_SPI->IFG&BIT1));//wait for the txbuf to be empty
     EUSCI_B0_SPI -> TXBUF = byte; //put in the data
 }
-
-
 
 /*
  * not currently necessary
@@ -59,7 +50,6 @@ void EUSCIB0_IRQHandler()
     if(EUSCI_B0_SPI->IV==0x4)//if the interrupt is of the correct type (TX)
     {
         while((EUSCI_B0_SPI->STATW&EUSCI_B_STATW_SPI_BUSY));//wait for it to finish writing
-        //P4->OUT&=~BIT1;//set the cs to low
     }
     EUSCI_B0_SPI->IFG&=~BIT1;//reset flag
 }

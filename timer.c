815f391f57d0e16393d9b2fa95e2f0b13095dea2
christian <christian@ThinkPadDifferently>
2020-05-07 17:24:46 -0700

    Magic Numbers because the msp432p401r doesn't work properly.

--- timer.c
+++ timer.c
@@ -28,34 +28,39 @@ void TA0_0_IRQHandler(void)
     TIMER_A0->CCR[0] += 3750;
 }
 
-// Timer A0's CCR1 interrupt service routine
+// Timer A0's CCR1 and CCR2 interrupt service routine
 void TA0_N_IRQHandler(void)
 {
-    static uint16_t triangle_val = 1;
-    static uint8_t counting_up = 1;
+    static uint16_t triangle_val = 0;
+    static uint8_t counting_up = 0;
 
-    if (TIMER_A0->IV == 2) // CCR1 source of interrupt
+    if(TIMER_A0->IV == 4)       // If interrupt source is Timer A0 CCR2
+      {
+          uint16_t voltage_val;
+          TIMER_A0->CCR[2] += 61;
+          if(triangle_val == 620)
+          {
+              counting_up = 0;
+          }
+          if(triangle_val == 0)
+          {
+              counting_up = 1;
+          }
+          counting_up ? (triangle_val+=20) : (triangle_val-=20);
+
+          voltage_val = 310 + triangle_val;
+          TIMER_A0->CCTL[2] &= ~TIMER_A_CCTLN_CCIFG; // clears capture/compare interrupt flag
+          write_DAC(voltage_val); // output high 2Vpp with 1V DC offset
+
+      }
+
+    else if (TIMER_A0->IV == 2) // CCR1 source of interrupt
     {
         TIMER_A0->CCTL[1] &= ~TIMER_A_CCTLN_CCIFG; // clears capture/compare interrupt flag
         write_DAC(310); // output low 2Vpp with 1V DC offset
         TIMER_A0->CCR[1] += 3750;
     }
 
-    if(TIMER_A0->IV == 4)       // If interrupt source is Timer A0 CCR2
-    {
-        if(triangle_val == 650)
-        {
-            counting_up = 0;
-        }
-        if(triangle_val == 0)
-        {
-            counting_up = 1;
-        }
-        counting_up ? (triangle_val++) : (triangle_val--);
 
-        TIMER_A0->CCTL[2] &= ~TIMER_A_CCTLN_CCIFG; // clears capture/compare interrupt flag
-        write_DAC(310+triangle_val); // output high 2Vpp with 1V DC offset
-        TIMER_A0->CCR[2] += 6;
-    }
 
 }

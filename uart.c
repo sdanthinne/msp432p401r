f623f0048ff9188ef3ced92b5b6e18a07649c3cc
Alexander Knapen <alexander.w.knapen@gmail.com>
2020-05-18 08:39:52 -0700

    actually added config

--- uart.c
+++ uart.c
@@ -4,5 +4,15 @@
  *  Created on: May 18, 2020
  *      Author: Alexander
  */
+#include "msp.h"
 
+void setup_uart()
+{
+    EUSCI_A0->CTLW0 |= EUSCI_A_CTLW0_SWRST; // software reset enable
 
+    EUSCI_A0->CTLW0 |= EUSCI_A_CTLW0_SSEL__SMCLK | // select SMCLK as souce
+                       EUSCI_A_CTLW0_MODE_0 | // use UART mode
+                       EUSCI_A_CTLW0_PAR | // use even parity
+                       EUSCI_A_CTLW0_PEN; // enable parity
+
+}

949388a43a4597814b4abe747a285b7d5b2c47c7
Alexander Knapen <alexander.w.knapen@gmail.com>
2020-05-19 17:57:30 -0700

    first try at configuring uart

--- uart.c
+++ uart.c
@@ -2,17 +2,30 @@
  * uart.c
  *
  *  Created on: May 18, 2020
- *      Author: Alexander
+ *      Author: aknapen, crapp, sdanthinne
  */
 #include "msp.h"
 
 void setup_uart()
 {
-    EUSCI_A0->CTLW0 |= EUSCI_A_CTLW0_SWRST; // software reset enable
+    set_SM_DCO(); // Configure DCO and SMCLK settings
 
-    EUSCI_A0->CTLW0 |= EUSCI_A_CTLW0_SSEL__SMCLK | // select SMCLK as souce
+    EUSCI_A0->CTLW0 |= EUSCI_A_CTLW0_SWRST; // enable software reset
+
+    // Configure UART settings
+    EUSCI_A0->CTLW0 |= EUSCI_A_CTLW0_SSEL__SMCLK | // select SMCLK as source
                        EUSCI_A_CTLW0_MODE_0 | // use UART mode
                        EUSCI_A_CTLW0_PAR | // use even parity
                        EUSCI_A_CTLW0_PEN; // enable parity
 
+
+    // Baud rate configuration for 50 kbps with a 1.5 MHz clock
+
+    EUSCI_A0->BRW = 1; // set clock prescaler for baud rate generation
+
+    EUSCI_A0->MCTLW |= EUSCI_A_MCTLW_OS16; // enable oversampling mode
+    EUSCI_A0->MCTLW |= UCBRF__14; // First module stage select for modulation pattern
+    EUSCI_A0->MCTLW &= ~EUSCI_A_MCTLW_BRS_MASK; // clears second modulation stage select
+
+    EUSCI_A0->CTLW0 &= ~EUSCI_A_CTLW0_SWRST; // disable software reset
 }
--- uart.h
+++ uart.h
@@ -2,12 +2,14 @@
  * uart.h
  *
  *  Created on: May 18, 2020
- *      Author: Alexander
+ *      Author: aknapen, crapp, sdanthinne
  */
 
 #ifndef UART_H_
 #define UART_H_
 
+#define UCBRF__14 (14 << 4)
 
+void setup_uart();
 
 #endif /* UART_H_ */

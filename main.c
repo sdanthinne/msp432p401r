98d47cd0bfab18a4fce78f69cba6775d853d11ef
Alexander Knapen <alexander.w.knapen@gmail.com>
2020-05-27 13:10:28 -0700

    Transmit and receive working

--- main.c
+++ main.c
@@ -12,18 +12,21 @@ void main(void)
 {
     WDT_A->CTL = WDT_A_CTL_PW | WDT_A_CTL_HOLD;     // stop watchdog timer
     setup_uart(); // Configure use of UART
+    uint16_t read_val = 0;
 
-    P6->DIR |= BIT4;
-    P6->SEL0 &= ~BIT4;
-    P6->SEL1 &= ~BIT4;
-    P6->OUT |= BIT4;
+    P1->DIR |= BIT0; // set P1.0 output
+    P1->SEL0 &= ~BIT0;
+    P1->SEL1 &= ~BIT0; // select GPIO for P1.0
+    P1->OUT &= ~BIT0; // turn LED off initially
 
     while (1)
     {
-        while (~(EUSCI_A0->IFG & EUSCI_A_IFG_TXIFG) & EUSCI_A_IFG_TXIFG); // wait until TXBUF empty
-        EUSCI_A0->TXBUF = 0x82; // first half of 'A'
-        while (~(EUSCI_A0->IFG & EUSCI_A_IFG_TXIFG) & EUSCI_A_IFG_TXIFG); // wait until TXBUF empty
-        EUSCI_A0->TXBUF = 0x04; // second half of 'A'
-        P6->OUT ^= BIT4;
+        while (~(EUSCI_A0->IFG & EUSCI_A_IFG_RXIFG) & EUSCI_A_IFG_RXIFG); // wait until RXBUF is filled
+        //EUSCI_A0->TXBUF = 0x41; // transmit 'A'
+        read_val = EUSCI_A0->RXBUF;
+        if (read_val == 0x41) // if an A is read
+        {
+            P1->OUT ^= BIT0; // toggle LED
+        }
     }
 }

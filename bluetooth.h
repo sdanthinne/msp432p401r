b0e2f0571420a3d481e9f62f36ef6c59bd541495
Christian <Christian@192.168.86.51>
2020-05-30 18:23:49 -0700

    Started work on bluetooth implementation

---
+++ bluetooth.h
@@ -0,0 +1,23 @@
+/*
+ * uart.h
+ *
+ *  Created on: May 30, 2020
+ *      Author: crapp
+ */
+
+#ifndef UART_H_
+#define UART_H_
+
+#define UCBRF__2 (2 << 4)
+#define UCBRF__10 (10 << 4)
+#define UCBRF__12 (12 << 4)
+#define UCBRF__MAX (15 << 4)
+
+#define RETURN 0x0A // return character
+
+uint8_t is_ready; // Indicates whether or not to write to DAC
+uint32_t dac_in; // input value to DAC
+
+void setup_uart();
+
+#endif /* UART_H_ */
